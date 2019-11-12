9cf58baff0f5bde3f1106984f45f90289461fbea
kosmos434 <kosmos434@protonmail.com>
2019-11-11 20:18:35 -0600

    add flow control file

---
+++ flow.rb
@@ -0,0 +1,14 @@
+# puts 1 > 2
+# puts 1 < 2
+
+# puts 5 >= 4
+# puts 5 <= 4
+
+# # ? Are these equal?
+# puts 1 == 1
+# # ? Are these different?
+# puts 2 != 1
+
+# ! This is true though >:|
+puts 'cat' > 'dog'
+
--- methods2.rb
+++ methods2.rb
@@ -63,8 +63,56 @@
 
 
 
-lineWidth = 40
-puts "Table of Contents".center(lineWidth)
-puts "Chapter 1: Numbers".ljust(lineWidth / 2)  + "page 1".rjust(lineWidth / 2)
-puts "Chapter 2: Letters".ljust(lineWidth / 2)  + "page 1".rjust(lineWidth / 2)
-puts "Chapter 3: Variables".ljust(lineWidth / 2)  + "page 1".rjust(lineWidth / 2)
+# lineWidth = 40
+# puts "Table of Contents".center(lineWidth)
+# puts ''
+# puts "Chapter 1: Numbers".ljust(lineWidth / 2)  + "page 1".rjust(lineWidth / 2)
+# puts "Chapter 2: Letters".ljust(lineWidth / 2)  + "page 72".rjust(lineWidth / 2)
+# puts "Chapter 3: Variables".ljust(lineWidth / 2)  + "page 118".rjust(lineWidth / 2)
+
+
+# puts 5 ** 2
+# puts 5 ** 0.5
+# * Floor division (drop the decimal)
+# puts 7/3
+# * Modulus (drop the quotient, keep the remainder)
+# puts 7%3
+# puts 365%7
+
+
+# puts((5 - 2).abs)
+# puts((2 - 5).abs)
+
+
+# puts rand
+# puts rand
+# puts rand
+# puts(rand(10))
+# * Starts from zero, going up to but not including 1
+# puts(rand(1))
+# puts('The weatherman said there is a ' + rand(101).to_s + '% chance of rain')
+
+
+# # * Pseudo random number seed
+# srand 1776
+# puts(rand(100))
+# puts(rand(100))
+# puts(rand(100))
+# puts(rand(100))
+# puts(rand(100))
+# puts ''
+# puts(rand(100))
+# puts(rand(100))
+# puts(rand(100))
+# puts(rand(100))
+# puts(rand(100))
+# puts(rand(100))
+
+
+# puts(Math::PI)
+# puts(Math::E)
+# puts(Math.cos(Math::PI/3))
+# puts(Math.tan(Math::PI/4))
+# puts(Math.log(Math::E**2))
+# puts((1 + Math.sqrt(5))/2)
+
