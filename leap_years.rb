6c030c7ec54ddb99deaeb02e6a85f9bf2e64fd59
kosmos434 <kosmos434@protonmail.com>
2019-11-15 15:08:26 -0600

    add .select to leap year

--- leap_years.rb
+++ leap_years.rb
@@ -1,16 +1,25 @@
+# Simple leap year program
 
 
+# Remember to "to integer" the gets
+# * .to_i chomps the gets
+# * "Extraneous characters past the end of a valid number are ignored."
 puts 'Please enter a starting year'
-start = gets.chomp
+# start = gets.chomp.to_i
+start = gets.to_i
 
 puts 'Please enter an ending year'
-ending = gets.chomp
+# ending = gets.chomp.to_i
+ending = gets.to_i
 
 year_array = (start..ending).to_a
 
 
-# puts year_array
-
-for i in year_array do
-    puts i
+# * Use either .select or .each here
+year_array.select do |i|
+    if i % 400 == 0
+        puts "#{i} is a leap year"
+    elsif (i % 4 == 0) and !(i % 100 == 0)
+        puts "#{i} is a leap year"
+    end
 end
