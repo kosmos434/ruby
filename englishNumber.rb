78caa715499a806bfdbeb39733ea97c4922c04f8
kosmos434 <kosmos434@protonmail.com>
2019-11-18 20:45:14 -0600

    add english number program

---
+++ englishNumber.rb
@@ -0,0 +1,24 @@
+
+
+def numberText numberText
+    # Edge cases
+    if number < 0
+        return "Please enter a non-negative number"
+    end
+    if number == 0
+        return "Zero"
+    end
+end
+
+# Outputted string at the end
+# We append the written numbers to this first
+numString = ''
+
+
+onesPlace = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+
+tensPlace = ['ten', 'twenty', 'thirty', 'forty'. 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
+
+teensPlace = ['eleventeen', 'twelveteen','thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen']
+
+
