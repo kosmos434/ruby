2d429479c374da50496a4f5cbac16d35b5b3f154
kosmos434 <kosmos434@protonmail.com>
2019-11-18 20:45:32 -0600

    refactoring

---
+++ methods3.rb
@@ -0,0 +1,51 @@
+# def sayBanana
+#     puts "Banananaa"
+# end
+
+# sayBanana
+# sayBanana
+# sayBanana
+
+
+# paramenters/arguments
+# Parameters = parkninng spot, arguments = car
+# def sayBanana numberOfNanners
+#     puts "Bananana " * numberOfNanners
+# end
+
+# sayBanana 3
+
+
+# def doubleThis num
+#     numTimes2 = num * 2
+#     puts num.to_s + ' doubled is ' + numTimes2.to_s
+# end
+
+# doubleThis 44
+# # ! This is a local variable only
+# # puts numTimes2.to_s
+
+
+
+# def littlePest var
+#     var = nil
+#     puts 'HAHA! I ruined your variable!'
+# end
+
+# var = "Everything is fine"
+# littlePest var
+# puts var
+
+
+# # * Puts always returns nil
+# returnVal = puts 'This puts returned:'
+# puts returnVal
+
+
+# def sayBanana numberOfNanners
+#     puts "Bananana " * numberOfNanners
+# end
+
+# sayBanana 2
+
+
