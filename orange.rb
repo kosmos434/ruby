42430e3804f4a54dfa0fa1eeef046e321660f41c
kosmos434 <kosmos434@protonmail.com>
2019-11-24 12:43:52 -0600

    add orange pick program

--- orange.rb
+++ orange.rb
@@ -3,11 +3,10 @@ class OrangeTree
     def initialize
         @orangeCount = 0
         @age = 0
-        @height = 0
-        @lifespan = 100
+        @height = 1
+        @lifespan = 10
 
         puts 'A seed sprouts from the earth.'
-
     end
 
     def pickAnOrange
@@ -19,20 +18,89 @@ class OrangeTree
         end
     end
 
-
-
-    private
-
-    def oneYearPasses
-        # TODO
-        # Season check
-        # Fruit move
-        # Height increase by a set amount
-        # Age increase
-        # Orangecount reset?
-        @age += 1
+    def height
+        puts 'The tree height is: ' + @height.to_s + ' feet tall.'
     end
 
-    def makeOranges
-        if age > 50
-            
+    def oneYearPasses
+        # Age increase
+        if @age >= @lifespan
+            puts 'The tree has lived long \'nuff. It\'s totally dead now.'
+            exit
+        else
+            @age += 1
+            puts 'The tree is now ' + @age.to_s + ' years old.'
+        end
+
+        # Fruit growth
+        if (@age > 5) && !(dead?)
+            @orangeCount = rand(10)
+            puts 'There are now ' + @orangeCount.to_s + ' oranges on the tree.'
+        elsif (@age > 2) && !(dead?)
+            # a new orange for every year, after five years
+            @orangeCount = rand(3)
+            puts 'There are now ' + @orangeCount.to_s + ' oranges on the tree.'
+        else
+            @orangeCount = 0
+        end
+
+        # Height increase
+        if !(dead?)
+            # ? Need a simple growth formula
+            @height += @age
+        end
+    end
+
+    
+    private
+
+    def dead?
+        @age >= @lifespan
+    end
+
+end
+
+
+twee = OrangeTree.new
+
+twee.pickAnOrange
+twee.oneYearPasses
+twee.height
+twee.pickAnOrange
+twee.oneYearPasses
+twee.pickAnOrange
+twee.oneYearPasses
+twee.pickAnOrange
+twee.oneYearPasses
+twee.pickAnOrange
+twee.oneYearPasses
+twee.pickAnOrange
+twee.height
+twee.oneYearPasses
+twee.pickAnOrange
+twee.oneYearPasses
+twee.pickAnOrange
+twee.oneYearPasses
+twee.height
+twee.pickAnOrange
+twee.oneYearPasses
+twee.pickAnOrange
+twee.oneYearPasses
+twee.height
+twee.pickAnOrange
+twee.oneYearPasses
+twee.pickAnOrange
+twee.oneYearPasses
+twee.pickAnOrange
+twee.height
+twee.oneYearPasses
+twee.pickAnOrange
+twee.oneYearPasses
+twee.pickAnOrange
+twee.height
+twee.oneYearPasses
+twee.pickAnOrange
+twee.oneYearPasses
+twee.height
+twee.pickAnOrange
+twee.oneYearPasses
